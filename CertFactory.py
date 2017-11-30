c28c7e0747fc5576880729f10ced213248bc7868
Prashanth V <31573243+pvenkat7jhu@users.noreply.github.com>
2017-11-30 17:55:43 -0500

    Create CertFactory.py

---
+++ CertFactory.py
@@ -0,0 +1,77 @@
+
+
+# Server - 20174.1.666.45      Client - 20174.1.666.46
+
+def getPrivateKeyForAddr(addr):
+    if addr == "20174.1.666.45":
+        with open("/root/Downloads/prashanth.key") as fp:
+            private_key_user = fp.read()
+        return str.encode(private_key_user)
+
+    if addr == "20174.1.666.46":
+        with open("/root/Downloads/prashanth-client.key") as fp:
+            private_key_user = fp.read()
+        return str.encode(private_key_user)
+
+
+def getCertsForAddr(address):
+    if address == "20174.1.666.45":
+        with open("/root/Downloads/server_cert") as fp:
+            id_key_IACA = fp.read()
+        return str.encode(id_key_IACA)
+
+    if address == "20174.1.666.46":
+        with open("/root/Downloads/client_cert") as fp:
+            id_key_IACA = fp.read()
+        return str.encode(id_key_IACA)
+
+    if address == "20174.1.666":
+        with open("/root/Downloads/paas_signed.cert") as fp:
+            pub_key_IACA = fp.read()
+        return str.encode(pub_key_IACA)
+
+def getRootCert():
+    with open("/root/Downloads/root.crt") as fp:
+        root_key_IACA = fp.read()
+    return str.encode(root_key_IACA)
+
+
+#======================================================================================
+
+'''
+#Root Certificate
+def getRootCertsForAddr():
+    with open("/root/Downloads/root.crt") as fp:
+        root_key_IACA = fp.read()
+    return str.encode(root_key_IACA)
+
+#Intermediate Certificate
+def getCertsForAddr():
+    with open("/root/Downloads/paas_signed.cert") as fp:
+        pub_key_IACA = fp.read()
+    return str.encode(pub_key_IACA)
+
+#Client Identity Certificate
+def getClientIDCertsForAddr():
+    with open("/root/Downloads/client_cert") as fp:
+        id_key_IACA = fp.read()
+    return str.encode(id_key_IACA)
+
+#Client Private Key
+def getClientPrivateKeyForAddr():
+    with open("/root/Downloads/prashanth-client.key") as fp:
+        private_key_user = fp.read()
+    return str.encode(private_key_user)
+
+#Server Identity Certificate
+def getServerIDCertsForAddr():
+    with open("/root/Downloads/server_cert") as fp:
+        id_key_IACA = fp.read()
+    return str.encode(id_key_IACA)
+
+#Server Private key
+def getServerPrivateKeyForAddr():
+    with open("/root/Downloads/prashanth.key") as fp:
+        private_key_user = fp.read()
+    return str.encode(private_key_user)
+'''
