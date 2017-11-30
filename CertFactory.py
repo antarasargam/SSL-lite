e0a436736ef96b6bf4a0f02775f89bc2c5d39070
Prashanth V <31573243+pvenkat7jhu@users.noreply.github.com>
2017-11-30 18:20:13 -0500

    Update CertFactory.py

--- CertFactory.py
+++ CertFactory.py
@@ -3,29 +3,29 @@
 # Server - 20174.1.666.45      Client - 20174.1.666.46
 
 def getPrivateKeyForAddr(addr):
-    if addr == "20174.1.666.45":
+    if addr == "20174.1.666.45":    #Server Key
         with open("/root/Downloads/prashanth.key") as fp:
             private_key_user = fp.read()
         return str.encode(private_key_user)
 
-    if addr == "20174.1.666.46":
+    if addr == "20174.1.666.46":     #Client Key
         with open("/root/Downloads/prashanth-client.key") as fp:
             private_key_user = fp.read()
         return str.encode(private_key_user)
 
 
 def getCertsForAddr(address):
-    if address == "20174.1.666.45":
+    if address == "20174.1.666.45":   #Server ID Certificate
         with open("/root/Downloads/server_cert") as fp:
             id_key_IACA = fp.read()
         return str.encode(id_key_IACA)
 
-    if address == "20174.1.666.46":
+    if address == "20174.1.666.46":  #Client ID Certificate
         with open("/root/Downloads/client_cert") as fp:
             id_key_IACA = fp.read()
         return str.encode(id_key_IACA)
 
-    if address == "20174.1.666":
+    if address == "20174.1.666":   #Intermediate Certificate
         with open("/root/Downloads/paas_signed.cert") as fp:
             pub_key_IACA = fp.read()
         return str.encode(pub_key_IACA)
